c9fd45db0c2ac68a527cd13ccc127fe2bc72ce7e
midoriha yuzuki <midoriha_yuzuki@outlook.com>
2023-12-20 17:17:31 +0900

    feat: Viteプラグイン追加。vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { reactClickToComponent } from 'vite-plugin-react-click-to-component';
@@ -6,4 +7,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), reactClickToComponent(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./config/vitest-setup.ts'],
+  },
 });
